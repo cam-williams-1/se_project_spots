a74990a2eafc55f78ba15f1e8ab80b59ae0424e1
cam williams <cam.minimal@gmail.com>
2025-01-13 19:12:12 -0500

    Project 4 Stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,45 @@
 const initialCards = {
   objectOne: {
     name: "Coffee",
-    link: "https://unsplash.com/photos/a-cup-of-coffee-sitting-on-top-of-a-counter-z2PDbBmL4xE"
-  }
+    link: "https://unsplash.com/photos/a-cup-of-coffee-sitting-on-top-of-a-counter-z2PDbBmL4xE",
+  },
 
   objectTwo: {
     name: "Books",
-    link: "https://unsplash.com/photos/elegant-reading-room-with-library-and-armchair-for-relaxing-space-for-text-3d-rendering-imdnMdUDomE"
-  }
+    link: "https://unsplash.com/photos/elegant-reading-room-with-library-and-armchair-for-relaxing-space-for-text-3d-rendering-imdnMdUDomE",
+  },
 
   objectThree: {
     name: "Camping",
-    link: "https://unsplash.com/photos/orange-camping-tent-near-green-trees-y8Ngwq34_Ak"
-  }
+    link: "https://unsplash.com/photos/orange-camping-tent-near-green-trees-y8Ngwq34_Ak",
+  },
 
   objectFour: {
     name: "Community",
-    link: "https://unsplash.com/photos/hands-of-unrecognizable-senior-couple-with-their-grandaughter-planting-a-seedling-on-the-allotment-man-woman-and-a-small-girl-gardening-laQlvA_rdJU"
-  }
+    link: "https://unsplash.com/photos/hands-of-unrecognizable-senior-couple-with-their-grandaughter-planting-a-seedling-on-the-allotment-man-woman-and-a-small-girl-gardening-laQlvA_rdJU",
+  },
 
   objectFive: {
     name: "Music",
-    link: "https://unsplash.com/photos/a-group-of-people-that-are-on-stage-1zNJUusB55E"
-  }
+    link: "https://unsplash.com/photos/a-group-of-people-that-are-on-stage-1zNJUusB55E",
+  },
 
   objectSix: {
     name: "Coding",
-    link: "https://unsplash.com/photos/a-person-sitting-at-a-desk-with-two-computer-monitors-XY0Cx9QsK3s"
-  }
+    link: "https://unsplash.com/photos/a-person-sitting-at-a-desk-with-two-computer-monitors-XY0Cx9QsK3s",
+  },
 };
+
+const profileEditButton = document.querySelector(".profile__edit-btn");
+
+const editProfileModal = document.querySelector("#edit-profile-modal");
+
+profileEditButton.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_opened");
+});
+
+const profileModalClose = document.querySelector(".modal__close-btn");
+
+profileModalClose.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_opened");
+});
